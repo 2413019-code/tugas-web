377c5ab4d832a5f4f194b446a50a5c9b685f788b
2413019 <2413019@students.universitasmulia.ac.id>
2026-05-25 12:03:42 +0800

    feat:my learn page

--- app/navbar/component/Account.tsx
+++ app/navbar/component/Account.tsx
@@ -1,6 +1,7 @@
 export default function Account() {
   return (
     <div>
+         <h2 className="text-amber-50">Account</h2>
       <div className="flex items-center gap-2">
         <img src="/user-circle.svg" alt="Design" className="w-5 h-5" />
         <p>Profile</p>
